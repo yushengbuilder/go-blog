042378ccc0f181e6ed236fa24cf7ad765fa2ce63
yusheng <duxiu1994@gmail.com>
2019-05-07 14:58:09 +0800

    添加注释

--- controllers/PostController.go
+++ controllers/PostController.go
@@ -9,30 +9,27 @@ import (
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	var postModel models.Post
-	posts := postModel.GetPosts()
-	s, err := template.ParseFiles("./views/index.html")
+	r.ParseForm()//解析请求参数
+	var postModel models.Post//获取文章model
+	posts := postModel.GetPosts()//获取所有文章
+	s, err := template.ParseFiles("./views/index.html")//解析模版
 	if err != nil {
 		fmt.Println(err)
 	}
-	s.Execute(w, posts)
+	s.Execute(w, posts)//合并模版
 }
 
 func Info(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	id, err := strconv.Atoi(r.Form["id"][0])
+	r.ParseForm()//解析请求参数
+	id, err := strconv.Atoi(r.Form["id"][0])//转换请求中id为数字
 	if err != nil {
 		fmt.Println(err)
 	}
-	var postModel models.Post
-	post := postModel.GetPost(id)
+	var postModel models.Post//获取文章model
+	post := postModel.GetPost(id)//获取文章详情
+	s, err := template.ParseFiles("./views/post.html")//解析模版
 	if err != nil {
 		fmt.Println(err)
 	}
-	s, err := template.ParseFiles("./views/post.html")
-	if err != nil {
-		fmt.Println(err)
-	}
-	s.Execute(w, post)
+	s.Execute(w, post)//合并模版
 }
--- models/post.go
+++ models/post.go
@@ -15,15 +15,15 @@ type Post struct {
 
 func (p *Post) GetPosts() []Post {
 	var posts []Post
-	posts = readFakeDataFile("./fakeData/posts.json")
+	posts = readFakeDataFile("./fakeData/posts.json")//获取文件中的假数据，转换成json，之后替换成数据库查询
 	return posts
 }
 
 func (p *Post) GetPost(id int) Post {
 	var posts []Post
 	var post Post
-	posts = readFakeDataFile("./fakeData/posts.json")
-	for _, value := range posts {
+	posts = readFakeDataFile("./fakeData/posts.json")//获取文件中的假数据，转换成json，之后替换成数据库查询
+	for _, value := range posts {//循环筛选文章
 		if id == value.Id {
 			post = value
 		} else {
@@ -35,10 +35,10 @@ func (p *Post) GetPost(id int) Post {
 
 //此处之后替换成数据库查询
 func readFakeDataFile(path string) []Post {
-	fi, _ := os.Open(path)
-	defer fi.Close()
-	fd, _ := ioutil.ReadAll(fi)
+	fi, _ := os.Open(path)//打开文件
+	defer fi.Close()//关闭文件
+	fd, _ := ioutil.ReadAll(fi)//读取文件中所有内容
 	var posts []Post
-	json.Unmarshal([] byte (string(fd)), &posts)
+	json.Unmarshal([] byte (string(fd)), &posts)//将文件中的内容转换成json
 	return posts
 }
