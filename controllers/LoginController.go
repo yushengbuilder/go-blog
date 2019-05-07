c093cdb46e46761d952c370bbc0937c49f7454e9
yusheng <duxiu1994@gmail.com>
2019-05-07 17:42:30 +0800

    update

--- controllers/LoginController.go
+++ controllers/LoginController.go
@@ -1,5 +1,21 @@
 package controllers
 
-func login () {
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
+func Login (w http.ResponseWriter, r *http.Request) {
+	if r.Method =="GET" {
+		t, err := template.ParseFiles("./views/login.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println(r.Form.Get("name"))
+		fmt.Println(r.Form.Get("password"))
+	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,6 +9,8 @@ func Route() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) //处理静态文件
 
 	http.HandleFunc("/", controllers.Index)      //首页
+	http.HandleFunc("/login", controllers.Login)      //首页
 	http.HandleFunc("/post", controllers.Info)   //详情页
 	http.HandleFunc("/about", controllers.About) //关于页
+
 }
